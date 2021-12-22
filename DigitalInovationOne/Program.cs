4d3a92c9b5474854b3d8a1865cab252bcfa4961d
flaviocarvalh0 <flavioalvespw@outlook.com>
2021-12-22 13:34:51 -0300

    first commit

---
+++ DigitalInovationOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for(int i = 0 ; i < numeroDeVezes; i++){
+                Console.WriteLine($"Bem-Vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
